1319b24c5a26310c7392e32d903d927072297c08
Michael Smith <michael.smith.ok@gmail.com>
2020-11-30 13:32:21 -0600

    Prepare package for pypi.org

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multiprocessing_tools", # Replace with your own username
+    version="1.0.0",
+    author="Michael Smith",
+    author_email="michael.smith.ok@gmail.com",
+    description="Simplified Python functions for multithreaded mapping and filtering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smwa/multiprocessing_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
